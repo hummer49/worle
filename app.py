05b1d3ec127482c728c69f91b75750193f67d782
Martin Portillo <maiporti@gmail.com>
2026-01-28 14:50:49 -0300

    Logica de funcion juego :: loop del juego

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 def verificar_palabra(
     palabra_objetivo:str, 
     palabra_ingresada:str
-) -> list:
+) -> list|None:
     """
     Docstring for verificar_palabra
     
@@ -17,7 +17,7 @@ def verificar_palabra(
     # Verficamos que ambas cadenas tengan la misma longitud
     if len(palabra_objetivo) != len(palabra_ingresada):
         print(f"La palabra ingresada <{palabra_ingresada}> no tiene la longitud adecuada")
-        return
+        return None # cambiar a raise Error o algo asi
     # creamos la lista para almacenar las cadenas
     palabra_verificada = []
     # recorremos la palabra ingresada
@@ -49,27 +49,28 @@ def juego():
     secreto = "perros" # a futuro elegir la palabra 'secreto' de un banco de palabras
     turnos = 5 # cantidad de turnos del jugador
     victoria = False # bandera que determina si el jugador gano o no
-    intentos = [] # lista para almacenar las listas de las palabras verificadas
+    lista_intentos = [] # lista para almacenar las listas de las palabras verificadas
 
     while victoria == False and turnos > 0:
+        # Mostrar la cantidad de turnos restantes
+        print(f"Quedan:\t{turnos} turnos" if turnos > 1 else f"Queda:\t{turnos} turno" )
+        # pedir la palabra al usuario
+        palabra_ingresada = input("Ingrese una palabra")
+        # verificamos palabra_ingresada, y la agregamos a lista_intentos
+        lista_intentos.append(verificar_palabra(secreto, palabra_ingresada))
+        # Mostramo la lista de intentos
+        imprimir_grilla(lista_intentos)
+        # Verificamos si el jugador gano
+        if secreto == palabra_ingresada:
+            victoria = True
+            break
+        # reducir cantidad de turnos
         turnos -= 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if victoria == True:
+        print("Ganaste!!")
+    else:
+        print("Perdiste")
 
 
 
