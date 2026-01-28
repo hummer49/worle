948dbdcf7a10947a82427ae6ed279f12e0f759c7
Martin Portillo <maiporti@gmail.com>
2026-01-28 14:25:21 -0300

    Estructura de funciones adicionales

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
 # app.py
 
+def verificar_palabra(
+    palabra_objetivo:str, 
+    palabra_ingresada:str
+) -> str:
+    """
+    Docstring for verificar_palabra
+    
+    :param palabra_objetivo: Description
+    :type palabra_objetivo: str
+    :param palabra_ingresada: Description
+    :type palabra_ingresada: str
+    :return: Description
+    :rtype: str
+    """
+    pass
+
+def imprimir_grilla(
+    grilla: list
+) -> None:
+    """
+    Docstring for imprimir_grilla
+    
+    :param grilla: Description
+    :type grilla: list
+    """
+    pass
+
+
+
+
+
 
 
 
 def juego():
-    pass
+    verificar_palabra('a',"b")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
